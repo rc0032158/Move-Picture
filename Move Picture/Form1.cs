81ea2793dde7df28871202ec90c6bd88741d1591
RC0032158 <RC0032158@CRO-24-07>
2018-09-11 10:24:02 +0100

    added picture

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -16,5 +16,25 @@ namespace Move_Picture
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_2(object sender, EventArgs e)
+        {
+
+        }
     }
 }
