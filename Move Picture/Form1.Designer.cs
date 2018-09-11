c939adbb2d1ffef37a3c11fe48f6878a696d3010
RC0032158 <RC0032158@CRO-24-07>
2018-09-11 10:33:51 +0100

    moving picture

--- Move Picture/Form1.Designer.cs
+++ Move Picture/Form1.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Move_Picture.Properties.Resources.rahford;
-            this.pictureBox1.Location = new System.Drawing.Point(87, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(102, 59);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(622, 363);
+            this.pictureBox1.Size = new System.Drawing.Size(387, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(610, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Move";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 564);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -36,5 +36,10 @@ namespace Move_Picture
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+        }
     }
 }
